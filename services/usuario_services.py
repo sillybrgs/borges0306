96df2ed3e8bdf718a976a211cbe1477df04b93c9
Pedro Borges <borges.pedro@aluno.ifsp.edu.br>
2025-06-03 16:03:38 -0300

    Atualização 16;03

--- services/usuario_services.py
+++ services/usuario_services.py
@@ -7,7 +7,7 @@ id_usuario = 0
 #função de atualização dos ids dos usuários
 def gerar_id_usuario():
     global id_usuario
-    id += 1
+    id_usuario += 1
     return id_usuario  
 def criar_usuario(dados):
     novo_usuario = Usuario(gerar_id_usuario(), dados['nome'], dados['email'], dados['senha'])
