4a668c52c8f5208e53e43e1ca7bf535fe2de43ad
Pedro Borges <borges.pedro@aluno.ifsp.edu.br>
2025-06-03 15:02:47 -0300

    Atualização 15:02

---
+++ services/usuario.services.py
@@ -0,0 +1,15 @@
+from models.usuario import Usuario
+
+usuario = []
+#Variavel de controle dos IDs dos usuários
+id_usuario = 0
+
+#função de atualização dos ids dos usuários
+def gerar_id_usuario():
+    global id_usuario
+    id += 1
+    return id_usuario  
+def criar_usuario(dados):
+    novo_usuario = Usuario(gerar_id_usuario(), dados['nome'], dados['email'], dados['senha'])
+    usuario.append(novo_usuario)
+    return novo_usuario.to_dict()
